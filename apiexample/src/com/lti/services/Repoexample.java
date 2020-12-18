bcaa57c300bccbf0ac6e078b42c233b718895651
Prachi <prachi2125namdev@gmail.com>
2020-12-18 10:12:27 +0530

    Commiting Repoexample for the first time

---
+++ apiexample/src/com/lti/services/Repoexample.java
@@ -0,0 +1,11 @@
+package com.lti.services;
+
+public class Repoexample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello I am Learning Github");
+	}
+
+}
